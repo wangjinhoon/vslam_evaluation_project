90f9ca935846a5bbd1789ee1358a88795c22c779
wangjinhoon <scn03170@gmail.com>
2022-06-15 17:03:24 +0900

    openmptest

--- Examples/mono_kitti.cc
+++ Examples/mono_kitti.cc
@@ -32,7 +32,7 @@
 #include<omp.h>
 #include"System.h"
 #include "easy/profiler.h"
-
+#include <omp.h>
 using namespace std;
 
 void LoadImages(const string &strSequence, vector<string> &vstrImageFilenames,
@@ -42,9 +42,11 @@ void LoadImages(const string &strSequence, vector<string> &vstrImageFilenames,
 int main(int argc, char **argv)
 {
   EASY_PROFILER_ENABLE
-  if(argc != 4)
+  if (argc != 4)
   {
-    cerr << endl << "Usage: ./mono_kitti path_to_vocabulary path_to_settings path_to_sequence" << endl;
+    cerr << endl
+         << "Usage: ./mono_kitti path_to_vocabulary path_to_settings path_to_sequence"
+         << endl;
     return 1;
   }
 
@@ -56,7 +58,7 @@ int main(int argc, char **argv)
   int nImages = vstrImageFilenames.size();
 
   // Create SLAM system. It initializes all system threads and gets ready to process frames.
-  ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::MONOCULAR,true);
+  ORB_SLAM2::System SLAM(argv[1], argv[2], ORB_SLAM2::System::MONOCULAR, true);
 
   // Vector for tracking time statistics
   vector<float> vTimesTrack;
@@ -71,70 +73,81 @@ int main(int argc, char **argv)
   cout << "Input number of images to detect: ";
   cin >> nImages;
   EASY_BLOCK("total imgs block", profiler::colors::Black)
-  for(int ni=0; ni<nImages; ni++)
-  {
-    EASY_BLOCK("single img block", profiler::colors::Yellow)
-    // Read image from file
-    im = cv::imread(vstrImageFilenames[ni],CV_LOAD_IMAGE_UNCHANGED);
-    double tframe = vTimestamps[ni];
-
-    if(im.empty())
+//#pragma omp parallel for
+//  {
+    for (int ni = 0; ni < nImages; ni++)
     {
-      cerr << endl << "Failed to load image at: " << vstrImageFilenames[ni] << endl;
-      return 1;
+      EASY_BLOCK("single img block", profiler::colors::Yellow)
+      // Read image from file
+      im = cv::imread(vstrImageFilenames[ni], CV_LOAD_IMAGE_UNCHANGED);
+      double tframe = vTimestamps[ni];
+
+      if (im.empty())
+      {
+        cerr << endl
+             << "Failed to load image at: " << vstrImageFilenames[ni] << endl;
+        return 1;
+      }
+
+#ifdef COMPILEDWITHC11
+      std::chrono::steady_clock::time_point t1 =
+        std::chrono::steady_clock::now();
+#else
+      std::chrono::monotonic_clock::time_point t1 =
+        std::chrono::monotonic_clock::now();
+#endif
+
+      // Pass the image to the SLAM system
+      SLAM.TrackMonocular(im, tframe);
+
+#ifdef COMPILEDWITHC11
+      std::chrono::steady_clock::time_point t2 =
+        std::chrono::steady_clock::now();
+#else
+      std::chrono::monotonic_clock::time_point t2 =
+        std::chrono::monotonic_clock::now();
+#endif
+
+      double ttrack =
+        std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1)
+          .count();
+
+      vTimesTrack[ni] = ttrack;
+
+      // Wait to load the next frame
+      double T = 0;
+      if (ni < nImages - 1)
+        T = vTimestamps[ni + 1] - tframe;
+      else if (ni > 0)
+        T = tframe - vTimestamps[ni - 1];
+
+      if (ttrack < T)
+        usleep((T - ttrack) * 1e6);
     }
+//  }
 
-#ifdef COMPILEDWITHC11
-    std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
-#else
-    std::chrono::monotonic_clock::time_point t1 = std::chrono::monotonic_clock::now();
-#endif
+    // Stop all threads
+    SLAM.Shutdown();
+    // ------------------------ end slam system --------------------------------
+    EASY_END_BLOCK
+    // Tracking time statistics
+    sort(vTimesTrack.begin(), vTimesTrack.end());
+    float totaltime = 0;
+    for (int ni = 0; ni < nImages; ni++)
+    {
+      totaltime += vTimesTrack[ni];
+    }
+    cout << "-------" << endl << endl;
+    cout << "median tracking time: " << vTimesTrack[nImages / 2] << endl;
+    cout << "mean tracking time: " << totaltime / nImages << endl;
 
-    // Pass the image to the SLAM system
-    SLAM.TrackMonocular(im,tframe);
-
-#ifdef COMPILEDWITHC11
-    std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
-#else
-    std::chrono::monotonic_clock::time_point t2 = std::chrono::monotonic_clock::now();
-#endif
-
-    double ttrack= std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
-
-    vTimesTrack[ni]=ttrack;
-
-    // Wait to load the next frame
-    double T=0;
-    if(ni<nImages-1)
-      T = vTimestamps[ni+1]-tframe;
-    else if(ni>0)
-      T = tframe-vTimestamps[ni-1];
-
-    if(ttrack<T)
-      usleep((T-ttrack)*1e6);
-
+    // Save camera trajectory
+    SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
+    profiler::dumpBlocksToFile("test_profile.prof");
+    return 0;
   }
 
-  // Stop all threads
-  SLAM.Shutdown();
-  // ------------------------ end slam system --------------------------------
-  EASY_END_BLOCK
-  // Tracking time statistics
-  sort(vTimesTrack.begin(),vTimesTrack.end());
-  float totaltime = 0;
-  for(int ni=0; ni<nImages; ni++)
-  {
-    totaltime+=vTimesTrack[ni];
-  }
-  cout << "-------" << endl << endl;
-  cout << "median tracking time: " << vTimesTrack[nImages/2] << endl;
-  cout << "mean tracking time: " << totaltime/nImages << endl;
 
-  // Save camera trajectory
-  SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
-  profiler::dumpBlocksToFile("test_profile.prof");
-  return 0;
-}
 
 void LoadImages(const string &strPathToSequence, vector<string> &vstrImageFilenames, vector<double> &vTimestamps)
 {
